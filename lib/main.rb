a1463281e5c67e6cbde345676a7ec1b681cb7422
raguanu <raguanu@sooraavali>
2010-01-01 03:11:39 +0530

    rendering works so so now.

--- lib/main.rb
+++ lib/main.rb
@@ -1,12 +1,23 @@
 require 'factory'
+require 'renderers/tamil'
 
-note_buckets = Factory.instance.create_notes("p; pmr grs n, pnsrn")
+note_buckets = Factory.instance.create_notes("p; pmr grs; n, pnsrn")
 puts note_buckets.to_s
 
-require 'rubygems'
-require 'RMagick'
-include Magick
+#require 'rubygems'
+#require 'RMagick'
+#include Magick
 
-test = ImageList.new("/tmp/thukkada_trial.png")
-test.display
+#canvas = ImageList.new
+#canvas.new_image(1000, 1000, HatchFill.new('white', 'lightcyan2', 20))
+#
+#score = ImageList.new('./renderers/data/tamil/ni.png', './renderers/data/tamil/sa.png')
+#score.append(false)
+#
+#canvas << score.append(false)
+#canvas.flatten_images.write '/tmp/test.png'
+##images.display
+#exit
+
+Tamil.new(note_buckets).render
 exit
